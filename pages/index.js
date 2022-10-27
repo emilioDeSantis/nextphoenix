1f33fdaa51d9888f930c5dcb282243010248b5a5
emilio <emilio5139@gmail.com>
2022-10-27 16:18:40 -0400

    fixed keys

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-// import NumberedList from "./components/NumberedList";
+import NumberedList from "./components/NumberedList";
 
 function Home() {
     return (
