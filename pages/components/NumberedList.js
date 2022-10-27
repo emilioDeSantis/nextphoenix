ee76eace0950118c60f1f5155b700fcac0581190
emilio <emilio5139@gmail.com>
2022-10-27 16:04:54 -0400

    fixed keys

--- pages/components/NumberedList.js
+++ pages/components/NumberedList.js
@@ -1,30 +1,30 @@
-import { Fragment } from "react";
+// import { Fragment } from "react";
 
-export default function NumberedList({ title, list }) {
-    return (
-        <Fragment>
-            <h3>{title}</h3>
-            <hr />
-            {list.map((element, index) => {
-                return (
-                    <Fragment key={element.title}>
-                        <section className="container">
-                            <div>{"0" + (index + 1)}</div>
-                            <div>
-                                <h4>{element.title}</h4>
-                                <p>{element.text}</p>
-                            </div>
-                            <style jsx>{`
-                                .container {
-                                    display: flex;
-                                    flex-direction: row;
-                                }
-                            `}</style>
-                        </section>
-                        <hr />
-                    </Fragment>
-                );
-            })}
-        </Fragment>
-    );
-}
+// export default function NumberedList({ title, list }) {
+//     return (
+//         <Fragment>
+//             <h3>{title}</h3>
+//             <hr />
+//             {list.map((element, index) => {
+//                 return (
+//                     <Fragment key={element.title}>
+//                         <section className="container">
+//                             <div>{"0" + (index + 1)}</div>
+//                             <div>
+//                                 <h4>{element.title}</h4>
+//                                 <p>{element.text}</p>
+//                             </div>
+//                             <style jsx>{`
+//                                 .container {
+//                                     display: flex;
+//                                     flex-direction: row;
+//                                 }
+//                             `}</style>
+//                         </section>
+//                         <hr />
+//                     </Fragment>
+//                 );
+//             })}
+//         </Fragment>
+//     );
+// }
---
+++ pages/theme.js
@@ -0,0 +1,10 @@
+export const color = {
+    background: "#fff",
+    theme: "#1E13FF",
+}
+
+export const dimension = {
+    sideMargin: "0.8rem",
+    navHeight: '3rem',
+    borderRadius: '0.2rem',
+}
