b5ff58a6ede20c4d3e99b586fa50ca2f39a30806
emilio <emilio5139@gmail.com>
2022-10-27 16:25:08 -0400

    fixed keys

--- pages/components/NumberedList.js
+++ pages/components/NumberedList.js
@@ -1,40 +1,30 @@
-export default function NumberedList() {
+import { Fragment } from "react";
+
+export default function NumberedList({ title, list }) {
     return (
-        <div className='container'>
-            
-        </div>
-    )
+        <Fragment>
+            <h3>{title}</h3>
+            <hr />
+            {/* {list.map((element, index) => {
+                return (
+                    <Fragment key={element.title}>
+                        <section className="container">
+                            <div>{"0" + (index + 1)}</div>
+                            <div>
+                                <h4>{element.title}</h4>
+                                <p>{element.text}</p>
+                            </div>
+                            <style jsx>{`
+                                .container {
+                                    display: flex;
+                                    flex-direction: row;
+                                }
+                            `}</style>
+                        </section>
+                        <hr />
+                    </Fragment>
+                );
+            })} */}
+        </Fragment>
+    );
 }
-
-
-
-// import { Fragment } from "react";
-
-// export default function NumberedList({ title, list }) {
-//     return (
-//         <Fragment>
-//             <h3>{title}</h3>
-//             <hr />
-//             {list.map((element, index) => {
-//                 return (
-//                     <Fragment key={element.title}>
-//                         <section className="container">
-//                             <div>{"0" + (index + 1)}</div>
-//                             <div>
-//                                 <h4>{element.title}</h4>
-//                                 <p>{element.text}</p>
-//                             </div>
-//                             <style jsx>{`
-//                                 .container {
-//                                     display: flex;
-//                                     flex-direction: row;
-//                                 }
-//                             `}</style>
-//                         </section>
-//                         <hr />
-//                     </Fragment>
-//                 );
-//             })}
-//         </Fragment>
-//     );
-// }
